c787af887d0932458860fd05a7ae53efa2098e58
Ivan Ursatii <bleiriam@gmail.com>
2026-06-30 19:45:36 +0300

    lesson 7.2

--- lesson_7.py
+++ lesson_7.py
@@ -337,12 +337,28 @@
 
 
 #ДЗ 7.1
-def say_hi(name, age):
-    return f"Hi. My name is {name} and I'm {age} years old"
+# def say_hi(name, age):
+#     return f"Hi. My name is {name} and I'm {age} years old"
 
-assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
-assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
-print('ОК')
+# assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+# assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+# print('ОК')
+
+#ДЗ 7.2
+
+def correct_sentence(text):
+    q = text[0].upper() + text[1:]
+    if q[-1] != ".":
+        q += "."
+    return "".join(q)
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", "Test1"
+assert correct_sentence("hello") == "Hello.", "Test2"
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", "Test3"
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", "Test4"
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", "Test5"
+print("ОК")
 
 
 #
