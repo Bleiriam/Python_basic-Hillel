90e4d4fb556ad2a8bcd13a436f25230d4ceaf15f
Ivan Ursatii <bleiriam@gmail.com>
2026-07-24 11:57:32 +0300

    lesson 14.2

--- lesson_13.py
+++ lesson_13.py
@@ -117,73 +117,129 @@
 
 # ДЗ 14.1. Група студентів
 
-class Human:
+# class Human:
 
-    def __init__(self, gender, age, first_name, last_name):
-        self.gender = gender
-        self.age = age
-        self.first_name = first_name
-        self.last_name = last_name
+#     def __init__(self, gender, age, first_name, last_name):
+#         self.gender = gender
+#         self.age = age
+#         self.first_name = first_name
+#         self.last_name = last_name
 
-    def __str__(self):
-        return f"Human: Gender: {self.gender}, age - {self.age} name: {self.first_name} last name: {self.last_name}"
-
-
-class Student(Human):
-
-    def __init__(self, gender, age, first_name, last_name, record_book):
-        super().__init__(gender, age, first_name, last_name)
-        self.record_book = record_book
-
-    def __str__(self):
-        return f"{super().__str__()}, record_book: {self.record_book}"
+#     def __str__(self):
+#         return f"Human: Gender: {self.gender}, age - {self.age} name: {self.first_name} last name: {self.last_name}"
 
 
-class Group:
+# class Student(Human):
 
-    def __init__(self, number):
-        self.number = number
-        self.group = set()
+#     def __init__(self, gender, age, first_name, last_name, record_book):
+#         super().__init__(gender, age, first_name, last_name)
+#         self.record_book = record_book
 
-    def add_student(self, student):
-        self.group.add(student)
-
-    def delete_student(self, last_name):
-        student = self.find_student(last_name)
-        if student is not None:
-            self.group.discard(student)
-
-    def find_student(self, last_name):
-        for i in self.group:
-            if i.last_name == last_name:
-                return i
-        return None
-
-    def __str__(self):
-        all_students = ""
-        for www in self.group:
-            all_students += str(www) + "\n"
-        return f"Number:{self.number}\n {all_students} "
+#     def __str__(self):
+#         return f"{super().__str__()}, record_book: {self.record_book}"
 
 
-st1 = Student("Male", 30, "Steve", "Jobs", "AN142")
-st2 = Student("Female", 25, "Liza", "Taylor", "AN145")
-gr = Group("PD1")
-gr.add_student(st1)
-gr.add_student(st2)
-print(gr)
-assert str(gr.find_student("Jobs")) == str(st1), "Test1"
-assert gr.find_student("Jobs2") is None, "Test2"
-assert (
-    isinstance(gr.find_student("Jobs"), Student) is True
-), "Метод пошуку повинен повертати екземпляр"
+# class Group:
 
-gr.delete_student("Taylor")
-print(gr)  # Only one student
+#     def __init__(self, number):
+#         self.number = number
+#         self.group = set()
 
-gr.delete_student("Taylor")  # No error!
+#     def add_student(self, student):
+#         self.group.add(student)
+
+#     def delete_student(self, last_name):
+#         student = self.find_student(last_name)
+#         if student is not None:
+#             self.group.discard(student)
+
+#     def find_student(self, last_name):
+#         for i in self.group:
+#             if i.last_name == last_name:
+#                 return i
+#         return None
+
+#     def __str__(self):
+#         all_students = ""
+#         for www in self.group:
+#             all_students += str(www) + "\n"
+#         return f"Number:{self.number}\n {all_students} "
 
 
+# st1 = Student("Male", 30, "Steve", "Jobs", "AN142")
+# st2 = Student("Female", 25, "Liza", "Taylor", "AN145")
+# gr = Group("PD1")
+# gr.add_student(st1)
+# gr.add_student(st2)
+# print(gr)
+# assert str(gr.find_student("Jobs")) == str(st1), "Test1"
+# assert gr.find_student("Jobs2") is None, "Test2"
+# assert (
+#     isinstance(gr.find_student("Jobs"), Student) is True
+# ), "Метод пошуку повинен повертати екземпляр"
+
+# gr.delete_student("Taylor")
+# print(gr)  # Only one student
+
+# gr.delete_student("Taylor")  # No error!
+
+#ДЗ 14.2. Клас "Цифровий лічильник"
+
+class Counter:
+
+    def __init__(self, current=1, min_value=0, max_value=10):
+        self.current = current
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def set_current(self, start):
+        self.current = start
+
+    def set_max(self, max_max):
+        self.max_value = max_max
+
+    def set_min(self, min_min):
+        self.min_value = min_min
+        return min_min
+
+    def step_up(self):
+        if self.current == self.max_value:
+            raise ValueError("Досягнуто максимуму")
+        self.current = self.current + 1
+        return self.current
+
+    def step_down(self):
+        if self.current == self.min_value:
+            raise ValueError("Досягнуто мінімум")
+        self.current = self.current - 1
+        return self.current
+
+    def get_current(self):
+        return self.current
+
+
+counter = Counter()
+counter.set_current(7)
+counter.step_up()
+counter.step_up()
+counter.step_up()
+assert counter.get_current() == 10, "Test1"
+try:
+    counter.step_up()  # ValueError
+except ValueError as e:
+    print(e)  # Достигнут максимум
+assert counter.get_current() == 10, "Test2"
+
+counter.set_min(7)
+counter.step_down()
+counter.step_down()
+counter.step_down()
+assert counter.get_current() == 7, "Test3"
+try:
+    counter.step_down()  # ValueError
+except ValueError as e:
+    print(e)  # Достигнут минимум
+assert counter.get_current() == 7, "Test4"
 
 
 
